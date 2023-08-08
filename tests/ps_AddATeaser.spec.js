b225493ce7912a105f2ff2121ad8e6837a1af21b
Akhil <akhiltheguitarist@gmail.com>
2023-08-08 22:28:59 +0530

    Modified add a teaser script

--- tests/ps_AddATeaser.spec.js
+++ tests/ps_AddATeaser.spec.js
@@ -1,4 +1,6 @@
 import { test, expect } from "@playwright/test";
+const teaserProps = require("../input.json");
+import { addAComponent, goToAPage } from "../utils/commonMethods";
 
 test.beforeEach(async ({ page }) => {
   await page.goto('/');
@@ -6,44 +8,40 @@ test.beforeEach(async ({ page }) => {
 
 test("test", async ({ page }) => {
     // test.setTimeout(120000);
- const config = {
+  const config = {
     siteName: 'WKND Site',
     siteId: 'wknd',
-    pageId: 'about-us',
-    elementType: 'Image',
-    label: 'TEST Image',
-    teaserText: 'Welcome to AEM website',
+    pageId: teaserProps["Page name"].toLowerCase().replaceAll(/ /g, "-"),
+    elementType: 'Teaser',
+    teaserText:  teaserProps["Text for teaser"],
   }
 
-    await page.goto(`http://localhost:4502/editor.html/content/${config.siteId}/language-masters/en/${config.pageId}.html`);
+  await goToAPage(page, config);
+  await addAComponent(page, config);
+  await page.reload();
+  await page.waitForLoadState("networkidle");
 
-    await page.locator('//*[@data-type="Editable" and @role="link" and @data-text="Drag components here"]').dblclick();
+  const newTeasers = await page.locator('//*[@role="link" and @title="Teaser"]').all();
+  await newTeasers[newTeasers.length-1].dblclick();
+  await page.locator('//coral-tab-label[contains(text(),"Text")]').click()
+  // await page.locator('//label[text()="Pretitle"]/following-sibling::input').type('vaevavhaev');
+  await page.getByLabel('Get title from linked page').uncheck();
+  await page.locator('//label[text()="Title"]/following-sibling::input').fill('');
+  await page.locator('//label[text()="Title"]/following-sibling::input').type(config.teaserText);
+  await page.locator('//coral-tab-label[contains(text(),"Asset")]').click()
+  await page.getByLabel('Inherit featured image from page').uncheck();
+  // await page.waitForLoadState('networkidle');
+  const fileChooserPromise = page.waitForEvent('filechooser');
+  await page.locator('a', { hasText: 'browse' }).click();
+  const fileChooser = await fileChooserPromise;
+  await fileChooser.setFiles('image.jpg');
 
-await page.locator('//coral-selectlist-item[contains(text(),"Teaser")]').click();
-
-await page.reload()
-await page.waitForLoadState('networkidle');
-const newTeasers = await page.locator('//*[@role="link" and @title="Teaser"]').all();
-await newTeasers[newTeasers.length-1].dblclick();
-await page.locator('//coral-tab-label[contains(text(),"Text")]').click()
-// await page.locator('//label[text()="Pretitle"]/following-sibling::input').type('vaevavhaev');
-await page.getByLabel('Get title from linked page').uncheck();
-await page.locator('//label[text()="Title"]/following-sibling::input').fill('');
-await page.locator('//label[text()="Title"]/following-sibling::input').type('Welcome to our site');
-await page.locator('//coral-tab-label[contains(text(),"Asset")]').click()
-await page.getByLabel('Inherit featured image from page').uncheck();
-// await page.waitForLoadState('networkidle');
-const fileChooserPromise = page.waitForEvent('filechooser');
-await page.locator('a', { hasText: 'browse' }).click();
-const fileChooser = await fileChooserPromise;
-await fileChooser.setFiles('image.jpg');
-
-// await page.waitForLoadState('networkidle');
-await page.getByLabel(`Don’t provide an alternative text`).check();
-await page.getByLabel(`Don’t provide an alternative text`).uncheck();
-await page.getByLabel(`Inherit from description of asset`).uncheck();
-await page.getByLabel(`Don’t provide an alternative text`).check();
+  // await page.waitForLoadState('networkidle');
+  await page.getByLabel(`Don’t provide an alternative text`).check();
+  await page.getByLabel(`Don’t provide an alternative text`).uncheck();
+  await page.getByLabel(`Inherit from description of asset`).uncheck();
+  await page.getByLabel(`Don’t provide an alternative text`).check();
 
 
-await page.keyboard.press('Enter');
+  await page.keyboard.press('Enter');
 });
